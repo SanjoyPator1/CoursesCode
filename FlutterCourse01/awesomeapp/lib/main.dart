ffaf78f860091ad142ad14660e0bc3401c329249
SanjoyPator <sanjoy18-378@aec.ac.in>
2021-04-30 19:13:16 +0530

    chapter 3 part 1 completed

--- FlutterCourse01/awesomeapp/lib/main.dart
+++ FlutterCourse01/awesomeapp/lib/main.dart
@@ -4,6 +4,9 @@ void main() {
   runApp(MaterialApp(
     title: "Awesome App", //show this on minimised
     home: HomePage(),
+    theme: ThemeData(
+      primaryColor: Colors.purple,
+    ),
   ));
 }
 
@@ -14,48 +17,44 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Align(
-          alignment: Alignment.center,  //align container by wrapping with align widget
-          child: Container(
-            color: Colors.black,
-            //width: MediaQuery.of(context).size.width,
-            //height: MediaQuery.of(context).size.height,
-            width: 200,
-            height: 400,
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    alignment: Alignment.center,
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    alignment: Alignment.center,
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    alignment: Alignment.center,
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                ],
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            // DrawerHeader(
+            //   child: Text(
+            //     "Hi I am drawer",
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+            //   decoration: BoxDecoration(color: Colors.purple),
+            // ),
+            UserAccountsDrawerHeader(
+              accountName: Text("Sanjoy"),
+              accountEmail: Text("sp@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80"),
               ),
             ),
-          ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("help@gmail.com"),
+              trailing: Icon(Icons.send),
+            )
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
       ),
     );
   }
