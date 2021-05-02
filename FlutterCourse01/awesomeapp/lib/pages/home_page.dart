761b6e6a2fd7b0eac15aa0407583500890b39819
SanjoyPator <sanjoy18-378@aec.ac.in>
2021-05-02 16:51:33 +0530

    shared prefrences completed

--- FlutterCourse01/awesomeapp/lib/pages/home_page.dart
+++ FlutterCourse01/awesomeapp/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:awesomeapp/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'drawer.dart';
 
@@ -42,6 +43,14 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Awesome App"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                Constants.prefs.setBool("LoggedIn", false);
+                Navigator.pushReplacementNamed(context, "/login");
+              })
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
